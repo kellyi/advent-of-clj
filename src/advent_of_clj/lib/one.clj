776554f57fd8a6576578691f7ca226e8dd5f992b
Kelly Innes <kinnes@gmail.com>
2018-12-01 11:50:35 -0500

    2018 1-2

--- src/advent_of_clj/lib/one.clj
+++ src/advent_of_clj/lib/one.clj
@@ -6,7 +6,34 @@
   (let [input (clojure.string/split (clojure.string/trim-newline (slurp "data/one.txt")) #"\n")]
     (map (fn [x] (read-string x)) input)))
 
+(defn solve-part-one
+  [n]
+  (reduce + n))
+
+(defn rotate
+  [[h & t]]
+  (concat t [h]))
+
+(defn solve-part-two-recursive
+  [l accumulator seen iterations]
+  (cond
+;;    (> iterations 1000) seen ;; for debugging
+    (.contains seen accumulator) accumulator
+    :else (recur (rotate l) (+ accumulator (first l)) (conj seen accumulator) (+ 1 iterations))))
+
+(defn solve-part-two
+  [l]
+  (solve-part-two-recursive l 0 [] 0))
+
+;; debugging test input
+;; (solve-part-two [1 -1])
+;; (solve-part-two [1 -2 3 1 1 -2])
+;; (solve-part-two [3 3 4 -2 -4])
+;; (solve-part-two [-6 3 8 5 -6])
+;; (solve-part-two [7 7 -2 -7 -4])
+
 (defn solve
   []
   (let [n (create-numbers)]
-  (println (reduce + n))))
+    (do (println (str "part one: " (solve-part-one n)))
+        (println (str "part two: " (solve-part-two n))))))
