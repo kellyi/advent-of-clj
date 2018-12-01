3da879eb81b54df510c9df8b93754649dfca3571
Kelly Innes <kinnes@gmail.com>
2018-11-30 23:57:18 -0500

    2017 1-1

--- src/advent_of_clj/lib/zero.clj
+++ src/advent_of_clj/lib/zero.clj
@@ -1,6 +1,28 @@
 (ns advent-of-clj.lib.zero
   (:gen-class))
 
+(defn create-numbers
+  []
+  (let [input (clojure.string/split (clojure.string/trim-newline (slurp "data/zero.txt")) #"")]
+  (map (fn [x] (read-string x)) input)))
+
+(defn sum-adjacent-identical-recursive
+  [[head nxt & tail] final accumulator]
+  (cond
+    (and (nil? nxt) (= head final)) (+ final accumulator)
+    (nil? nxt) accumulator
+    :else (cond
+            (and (= head nxt) (nil? tail)) (recur [nxt nil] final (+ accumulator head))
+            (nil? tail) (recur [nxt nil] final accumulator)
+            (= head nxt) (recur (vec (cons nxt tail)) final (+ accumulator head))
+            :else (recur (vec (cons nxt tail)) final accumulator))))
+
+(defn sum-adjacent-identical
+  [[head & tail]]
+  (sum-adjacent-identical-recursive (vec (cons head tail)) head 0))
+
+(def n (create-numbers))
+
 (defn solve
   []
-  (println "Solve"))
+  (println (str (sum-adjacent-identical (vec n)))))
