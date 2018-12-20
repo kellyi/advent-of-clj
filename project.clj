3a691b753931bb0c42aceaa3370dadfecbcdcb8b
Kelly Innes <kinnes@gmail.com>
2018-12-19 21:48:03 -0500

    Upgrade Clojure to 1.10

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject advent-of-clj "0.1.0-SNAPSHOT"
   :description "Programming puzzles in Clojure"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-time "0.15.1"]]
   :main ^:skip-aot advent-of-clj.core
   :target-path "target/%s"
